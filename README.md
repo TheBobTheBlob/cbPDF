adc12ec6039128821cb71124bb6aa64f77d2d295
Punit Turlapati <Punitsst@gmail.com>
2020-10-23 18:18:16 +0530

    Continuing work.

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
 # PDF Maker
 
-PDF Maker takes cbr, cbz, and cba files and converts them into a pdf. It can work for many files or a single one.
+PDF Maker takes cbr, cbz, and cba files and converts them into pdfs.
+
+### Requirements
+
+Python 3.6+
+
+Standard modules:
+* os
+* pathlib
+
+Other modules:
+* pyunpack
+* reportlab
 
 ### Setup
-Create one text files, `folders.txt` in the same location as `main.py`
-Delete any `.keep` file.
+
+Create one text files, `folders.txt` in the same location as `main.py`.
+Delete any `.keep` files.
 
 ### Usage
 
 In `folders.txt`, have the folders containing all files to be made into pdfs on the first line and the folder for pdfs to be outputted to on the second line. 
+
 ```
 Path to input directory
 Path to output directory
 ```
 
-All paths must contain forward slashs (/) only.
+Then run `main.py`.
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-import pathlib
-import zipfile
+iimport osimport pathlib
 
 import pyunpack
 import reportlab
 import PyPDF4
+
+# Variables
+file_types = [".cba", ".cbr", ".cbz"]
+temp_file = "unpacked"
+
+
+# Gets folders from folders.txt and converts them into Path objects
+with open("folders.txt") as file:
+    lines = file.readlines()
+    input_dir = pathlib.Path(lines[0].strip())
+    output_dir = pathlib.Path(lines[1].strip())
+
+
+# Gets all archives and adds their path to a list
+print(f"Getting archives from {os.fspath(input_dir)}")
+archives = []
+for file in input_dir.glob("*"):
+    if file.is_file() and file.suffix in file_types:
+        archives.append(file)
+print(f"Found {len(archives)} archives\n")
+
+
+os.mkdir(temp_file)
+
+finished = 1
+for file in archives:
+    print(f"{finished}/{len(archives)} Extracting {file.name}")
+    pyunpack.Archive(file).extractall(temp_file)
+    finished += 1
 
